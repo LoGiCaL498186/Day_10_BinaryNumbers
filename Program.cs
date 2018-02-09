9af33d174b281f72027f212e7b1f9155f2f587f9
David Ellison <David Ellison>
2018-02-09 17:39:15 -0500

    Day_10_BinaryNumbers_01

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        string binary = Convert.ToString(n, 2);
+
+        int count = binary.Split('0')
+                     .Select(num => num)
+                     .Max()
+                     .ToString()
+                     .Length;
+
+        Console.WriteLine(count);
+        Console.ReadLine();
+
+
+    }
+}
